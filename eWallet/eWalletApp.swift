3a703cff9edde43917c9f66b1117040eea6ef4dc
SSTedTan <ted_tan@s2023.ssts.edu.sg>
2024-08-17 13:38:23 +0800

    Initial Commit

---
+++ eWallet/eWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  eWalletApp.swift
+//  eWallet
+//
+//  Created by Ted Tan on 17/8/24.
+//
+
+import SwiftUI
+
+@main
+struct eWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
